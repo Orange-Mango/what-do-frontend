db532aac89f9f7b803f04ef30f4d372a50278bc1
Vasilis Douvaras <vasilisd@axis.com>
2020-04-04 16:55:45 +0200

    Add stacked activities

--- src/Activities/Activities.js
+++ src/Activities/Activities.js
@@ -1,60 +1,81 @@
-import React, { useState, useEffect } from "react";
-import { Button, Container, Grid } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import { Button, Container, Grid } from '@material-ui/core';
+import { useTransition, animated } from 'react-spring';
 
-import Activity from "./Activity/Activity";
+import Activity from './Activity/Activity';
 
 const Activities = () => {
-  const [activities, setActivities] = useState([{ description: "test" }]);
+  const [activities, setActivities] = useState([
+    { key: 0, description: 'a' },
+    { key: 1, description: 'b' },
+    { key: 2, description: 'c' }
+  ]);
+
+  const [index, setIndex] = useState(activities.length);
 
   useEffect(() => {
     function fetchData() {
-      fetch("http://78.82.184.12:55502/activities")
-        .then((response) => {
+      fetch('http://78.82.184.12:55502/activities')
+        .then(response => {
           return response.json();
         })
-        .then((data) => {
+        .then(data => {
           setActivities(data);
         });
     }
     fetchData();
   }, []);
 
-  const handleRemoveActivity = (index) => {
-    activities.splice(index, 1);
-    setActivities([...activities]);
+  const handleRemoveActivity = index => {
+    let newActivities = [...activities];
+    newActivities.pop();
+    setActivities(newActivities);
   };
 
   const handleAddActivity = () => {
-    setActivities([...activities, { description: "test" }]);
+    setIndex(index + 1);
+    let newActivities = [...activities];
+    newActivities.push({ key: index, description: 'new' });
+    setActivities(newActivities);
   };
 
-  console.warn(activities);
+  const transitions = useTransition(activities, p => p.key, {
+    from: { opacity: 0, transform: `translate3d(100%,0,0)` },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-150%,0,0)' }
+  });
+
   return (
     <Container>
       <Grid
         container
-        direction="column"
-        alignItems="center"
-        justify="center"
+        direction='column'
+        alignItems='center'
+        justify='center'
         spacing={3}
       >
         <Grid item>
           <Button
             onClick={handleAddActivity}
-            variant="contained"
-            color="primary"
+            variant='contained'
+            color='primary'
           >
             Add activity
           </Button>
         </Grid>
-        {activities.map(({ activity, index }) => (
-          <Grid item>
-            <Activity
-              removeActivity={handleRemoveActivity}
-              index={index}
-              activity={activity}
-            />
-          </Grid>
+        {transitions.map(({ item, props, key }) => (
+          <animated.div
+            style={{
+              ...props,
+              marginTop: '500px',
+              position: 'absolute'
+            }}
+            key={key}
+          >
+            <Grid item>
+              <Activity removeActivity={handleRemoveActivity} activity={item} />
+            </Grid>
+          </animated.div>
         ))}
       </Grid>
     </Container>
--- src/Activities/Activity/Activity.js
+++ src/Activities/Activity/Activity.js
@@ -1,50 +1,50 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import {
   Card,
   CardActions,
   CardContent,
   Button,
-  Typography,
-} from "@material-ui/core";
+  Typography
+} from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-    width: "30%",
+    width: '30%'
   },
   bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
   },
   title: {
-    fontSize: 14,
+    fontSize: 14
   },
   pos: {
-    marginBottom: 12,
-  },
+    marginBottom: 12
+  }
 });
 
-const Activity = ({ activity, index, removeActivity }) => {
+const Activity = ({ activity, removeActivity }) => {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
       <CardContent>
         <Typography
           className={classes.title}
-          color="textSecondary"
+          color='textSecondary'
           gutterBottom
         >
-          Category
+          Category {activity.key}
         </Typography>
 
-        <Typography variant="body2" component="p">
-          {activity && activity.description}
+        <Typography variant='body2' component='p'>
+          {activity.description}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button onClick={() => removeActivity(index)}>❌</Button>
+        <Button onClick={() => removeActivity(activity.key)}>❌</Button>
         <Button>❤️</Button>
       </CardActions>
     </Card>

5da9fdd92370c4035f6091ef588a38fc4ce96e9c
Vasilis Douvaras <vasilisdouvaras@gmail.com>
2020-04-05 00:37:06 +0200

    Update activity styling

--- src/Activities/Activity/Activity.js
+++ src/Activities/Activity/Activity.js
@@ -4,51 +4,60 @@ import {
   Card,
   CardActions,
   CardContent,
+  CardHeader,
+  Chip,
   Button,
-  Typography
+  Grid,
+  Typography,
 } from "@material-ui/core";
+import { animated } from "react-spring";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-    width: "30%"
+    width: "30%",
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  }
 });
 
-const Activity = ({ activity, removeActivity, likeActivity, tags }) => {
+const Activity = ({ key, props, activity, removeActivity, likeActivity }) => {
   const classes = useStyles();
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Category {activity.key}{" "}
-          {activity.tags && activity.tags.map(tag => tag).join(" ")}
-        </Typography>
+    <animated.div
+      key={key}
+      style={{
+        ...props,
+        marginTop: `${activity.key * 20}px`,
+        position: "absolute",
+      }}
+    >
+      <Grid item>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              {activity.tags && activity.tags[0]}
+            </Typography>
 
-        <Typography variant="body2" component="p">
-          {activity.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button onClick={() => removeActivity(activity.key)}>❌</Button>
-        <Button onClick={() => likeActivity(activity.key)}>❤️</Button>
-      </CardActions>
-    </Card>
+            <Typography variant="body2" component="p">
+              {activity.description}
+            </Typography>
+            {activity.tags &&
+              activity.tags.map((tag) => (
+                <Chip size="small" label={`${tag}`} />
+              ))}
+          </CardContent>
+          <CardActions>
+            <Grid container direction="row" justify="space-between">
+              <Button onClick={() => removeActivity(activity.key)}>❌</Button>
+              <Button onClick={() => likeActivity(activity.key)}>❤️</Button>
+            </Grid>
+          </CardActions>
+        </Card>
+      </Grid>
+    </animated.div>
   );
 };
 

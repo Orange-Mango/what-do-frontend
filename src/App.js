b99abc80b598ccad2f58dfd56ca986927b8ff065
Vasilis Douvaras <vasilisdouvaras@gmail.com>
2020-04-05 14:29:32 +0200

    Add Google auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 import Activities from "./Activities/Activities";
+import Login from "./Login/Login";
 
 function App() {
-  return <Activities />;
+  return (
+    <>
+      <Login />
+      <Activities />
+    </>
+  );
 }
 
 export default App;
